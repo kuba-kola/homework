718cd4969f36855b75bdf23219edcba18d557c9f
Yakau Trafimovich <5243212@gmail.com>
2022-03-09 14:08:43 +0100

    refactoring

--- task1.js
+++ task1.js
@@ -1,11 +1,10 @@
 const firstParam = +prompt("Введите первое значение");
     secondParam = +prompt("Введите второе значение");
 
-if ((Number.isFinite(firstParam) == true) && Number.isFinite(secondParam) == true && secondParam <= 10 && secondParam >= 2) {    
-    let res = firstParam.toString(secondParam);    
-    console.log(res);
+const correctParams = ((Number.isFinite(firstParam) == true) && Number.isFinite(secondParam) == true)
+if (correctParams && secondParam <= 10 && secondParam >= 2) {  
+    console.log(firstParam.toString(secondParam));
 }
 else {
     console.log('Некорректный ввод!');
 }
-
