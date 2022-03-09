a7173ef1f22972306a8bc40c454a5115446e8979
Yakau Trafimovich <5243212@gmail.com>
2022-03-09 16:33:23 +0100

    code style

--- task1.js
+++ task1.js
@@ -1,7 +1,8 @@
-const firstParam = +prompt("Введите первое значение");
-    secondParam = +prompt("Введите второе значение");
+const firstParam = +prompt('Введите первое значение');
+    secondParam = +prompt('Введите второе значение');
 
 const correctParams = ((Number.isFinite(firstParam) == true) && Number.isFinite(secondParam) == true)
+
 if (correctParams && secondParam <= 10 && secondParam >= 2) {  
     console.log(firstParam.toString(secondParam));
 }
