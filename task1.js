488f6e63350875739d777e7aa559b3bc047b1971
Yakau Trafimovich <5243212@gmail.com>
2022-03-09 16:56:35 +0100

    fix comments

--- task1.js
+++ task1.js
@@ -1,9 +1,10 @@
 const firstParam = +prompt('Введите первое значение');
     secondParam = +prompt('Введите второе значение');
 
-const correctParams = ((Number.isFinite(firstParam) == true) && Number.isFinite(secondParam) == true)
+const correctParams = (Number.isFinite && Number.isFinite)
+const compare = correctParams && secondParam <= 10 && secondParam >= 2;
 
-if (correctParams && secondParam <= 10 && secondParam >= 2) {  
+if (compare) {  
     console.log(firstParam.toString(secondParam));
 }
 else {
